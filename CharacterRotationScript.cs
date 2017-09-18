bafe2ff64d5c5f3f458c1affb5d45388d0d99957
Hamza Mian <31227117+HamzaMiann@users.noreply.github.com>
2017-09-18 13:05:44 -0400

    Add files via upload

---
+++ CharacterRotationScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterRotationScript : MonoBehaviour {
+
+	public GameObject MainCamera;
+	public float lookSpeed = 5f;
+	
+	// Update is called once per frame
+	void Update () {
+		Transform mainCamT = MainCamera.transform;
+		Vector3 mainCamPos = mainCamT.position;
+		Vector3 lookTarget = mainCamPos + (mainCamT.forward * 10f); // edit this
+		Vector3 thisPos = transform.position;
+		Vector3 lookDir = lookTarget - thisPos;
+		Quaternion lookRot = Quaternion.LookRotation (lookDir);
+
+		lookRot.x = 0;
+		lookRot.z = 0;
+
+		Quaternion newRotation = Quaternion.Lerp (transform.rotation, lookRot, Time.deltaTime * lookSpeed);
+		transform.rotation = newRotation;
+	}
+}
